7f30cf37f92890311259117f0086671a74c47da8
Eva F.M. <eafernandez@educarex.es>
2026-04-15 08:54:57 +0200

    2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import { PokemonList } from './pages/pokemon-list/pokemon-list';
 
 export const routes: Routes = [
-    { path: 'pokemonlist', component: PokemonList}
+    { path: 'pokemonlist', component: PokemonList},
+    { path: '', redirectTo: 'pokemonlist', pathMatch: 'full'}
 ];
